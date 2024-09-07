c3aff68bbca16c7f71b29cfff6b01b772c358a3f
sarwar12 <sarwarkhan.nahin@gmail.com>
2024-09-07 16:05:00 +0600

    second commit

---
+++ routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get("/test", function(){
+    return ["name"=>"Golam Sarwar", "channel"=>"Learn with GSarwar"];
+});
+
+Route::get('students', [StudentController::class, 'StudentList']);
