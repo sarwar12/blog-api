e1fe1f7df5be81d99119e0653ff8bba1f1ee6aea
sarwar12 <sarwarkhan.nahin@gmail.com>
2024-09-14 14:27:55 +0600

    add and check api

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
+use App\Http\Controllers\MemberController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -17,3 +18,4 @@ Route::post('add', [StudentController::class, 'addStudent']);
 Route::put('update', [StudentController::class, 'updateStudent']);  
 Route::delete('delete/{id}', [StudentController::class, 'deleteStudent']);  
 Route::get('search/{name}', [StudentController::class, 'searchStudent']);  
+Route::resource('member', MemberController::class);
